d6bb764e87e15a382763336fa616a968bfea7a9b
Wesley Scholl <wscholl@totalwine.com>
2023-09-29 17:06:54 -0400

    Added package-lock and updated index and tests

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,12 @@
-import { getInput, setFailed, setOutput } from "@actions/core";
-import { graphql } from "@octokit/graphql";
-
-interface Res {
-  addDiscussionComment: {
-    clientMutationId: string;
-    comment: {
-      id: string;
-      body: string;
-    };
-  };
-}
+import { getInput, setOutput } from "@actions/core";
 
 export async function run() {
   const token = getInput("GITHUB_TOKEN");
-  token === "INVALID_TOKEN" && setFailed("GitHub token missing or invalid, please enter a GITHUB_TOKEN");
-  const commentBody = getInput("comment_body");
-  const delay = getInput("delay_milliseconds");
-  const eventPayload = require(String(process.env.GITHUB_EVENT_PATH));
-  const discussionId = eventPayload.discussion.node_id;
-  discussionId === "INVALID_DISCUSSION_ID" && setFailed("Invalid or missing discussionId.");
-  await new Promise((f) => setTimeout(f, Number(delay)));
-
-  try {
-    const response: Res = await graphql({
-      query: `mutation {
-        addDiscussionComment(
-          input: { body: "${commentBody}", discussionId: "${discussionId}", clientMutationId: "1234" }
-        ) {
-          clientMutationId
-          comment {
-            id
-            body
-          }
-        }
-      }`,
-      headers: {
-        authorization: `token ${token}`,
-      },
-    });
-    await setOutput("discussionId", discussionId);
-    await setOutput("commentId", response?.addDiscussionComment?.comment?.id);
-    await setOutput("commentBody", response?.addDiscussionComment?.comment?.body);
-    await setOutput("clientMutationId", response?.addDiscussionComment?.clientMutationId);
-  } catch (error: any) {
-    setFailed(error.message);
-  }
+  const input1 = getInput("input1");
+  const input2 = getInput("input2");
+  await setOutput("token", token);
+  await setOutput("output1", input1);
+  await setOutput("output2", input2);
 }
 
-if (!process.env.JEST_WORKER_ID) {
-  run();
-}
+run();

8335b6a1aa9c6f7289332e012b1d82446a96355b
Wesley Scholl <128409641+wesleyscholl@users.noreply.github.com>
2024-12-30 11:54:50 -0500

    Debug: Log input and token values

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ export async function run() {
   const token = getInput("GITHUB_TOKEN");
   const input1 = getInput("input1");
   const input2 = getInput("input2");
+  console.log("token", token);
+  console.log("input1", input1);
+  console.log("input2", input2);
   await setOutput("token", token);
   await setOutput("output1", input1);
   await setOutput("output2", input2);
